f9089f4618891ce8becbd4ae679f6ec6e7889acd
saraahaddad <saraa.haddad@icloud.com>
2023-12-06 19:17:24 -0500

    added white tests method 2

--- currency-converter-in-java/src/test/WhiteBoxCurrencyConvertTest.java
+++ currency-converter-in-java/src/test/WhiteBoxCurrencyConvertTest.java
@@ -11,58 +11,9 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WhiteBoxCurrencyConvertTest {
-    ArrayList<Currency> currencies;
     @Test
-    @DisplayName("Test pour critères A et B")
-    public void testAB(){
-        String currency2 = "US Dollar";
-        String currency1 = "Euro";
-        assertEquals(1.073*100, MainWindow.convert(currency1,currency2,currencies,100.0));
-
-        currency2 = "Lebanese Pound";
-        currency1 = "Euro";
-        assertEquals(0.0, MainWindow.convert(currency1,currency2,currencies,100.0));
-
-        currency2 = "Indian Rupee";
-        currency1 = "Euro";
-        assertEquals(0.0, MainWindow.convert(currency1,currency2,currencies,100.0));
-
-        currency2 = "Euro";
-        currency1 = "Euro";
-        assertEquals(100.0, MainWindow.convert(currency1,currency2,currencies,100.0));
-
-        currency2 = "Euro";
-        currency1 = "US Dollar";
-        assertEquals(93.0, MainWindow.convert(currency1,currency2,currencies,100.0));
-
-        currency2 = "Euro";
-        currency1 = "CFA Franc";
-        assertEquals(0.0, MainWindow.convert(currency1,currency2,currencies,100.0));
-    }
-    @Test
-    @DisplayName("Tests pour critère C")
-    public void testC(){
-        String currency2;
-        String currency1;
-
-        // chemin 2
-        currency1 = "US Dollar";
-        currency2= "Euro";
-        assertEquals(93.0,MainWindow.convert(currency1,currency2,currencies, 100.0));
-
-        // chemin 3
-        currency1 = "Lebanese Pound";
-        currency2= "Indian Rupee";
-        assertEquals(0.0,MainWindow.convert(currency1,currency2,currencies, 1000.0));
-
-        // chemin 4
-        currency1 = "Indian Rupee";
-        currency2= "US Dollar";
-        assertEquals(0.0,MainWindow.convert(currency1,currency2,currencies, 1200000.0));
-    }
-
-    @BeforeEach
-    public void setUp(){
-        currencies = Currency.init();
+    @DisplayName("Test pour critères A et C")
+    public void testAC(){
+        assertEquals(135.0, Currency.convert(100.0, 1.35));
     }
 }
